c3aa1d1169054bdb1269f2a925f7c92c75773c5a
frank1iu <frankliupiye@icloud.com>
2019-11-06 12:49:46 -0800

    Revert "Deliverable 9: Split class into 2 classes"
    This reverts commit b69ecc67

--- src/main/racket/Tokenizer.java
+++ src/main/racket/Tokenizer.java
@@ -4,18 +4,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Tokenizer {
+    private String program;
     private ArrayList<String> split;
     private Thing thing;
-    private StringSplitter stringSplitter;
 
     public Tokenizer(String program) {
-        this.stringSplitter = new StringSplitter(program);
+        this.program = program;
     }
 
     // MODIFIES: this
     // EFFECTS: splits this.program and places into this.split
     public Tokenizer split() {
-        this.split = stringSplitter.split();
+        this.split = new ArrayList<String>(Arrays.asList(this.program.replaceAll("\\(", " ( ")
+                .replaceAll("\\)", " ) ")
+                .trim()
+                .split("\\s+")));
         return this;
     }
 
