d1c7615f75b59cf33b76f338949ba14435a8f96b
frank1iu <frankliupiye@icloud.com>
2019-11-10 21:40:02 -0800

    Update gitignore

--- src/main/network/TestGUI.java
+++ src/main/network/TestGUI.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLEncoder;
 
 public class TestGUI {
     private static final String url = "http://127.0.0.1:27999/exec?program=";
@@ -18,28 +17,27 @@ public class TestGUI {
     }
 
     public static void main(String[] args) throws IOException {
-        GUI.makeServer();
+        GUI.makeHttpServer();
         String result = httpGet(new URL(url + "(pow%202%204)"));
         assertEquals(result.trim(), "16");
         String result2 = httpGet(new URL(url + "(=%202%204)"));
         assertEquals(result2.trim(), "false");
+        // TODO delete next 4 lines after deliverable 10
+        String deliverable10 = httpGet(new URL(url + "deliverable10"));
+        System.out.println("The JSON obtained from the API is: " + deliverable10);
+        double a = Double.parseDouble(deliverable10.split("}")[0].split(":")[1]);
+        System.out.println("Value of a in the JSON is: " + a);
         System.exit(0);
     }
 
     private static String httpGet(URL url) throws IOException {
-
         try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-
             String line;
-
             StringBuilder sb = new StringBuilder();
-
             while ((line = br.readLine()) != null) {
-
                 sb.append(line);
                 sb.append(System.lineSeparator());
             }
-
             return sb.toString();
         }
     }
--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -5,18 +5,24 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import racket.AbstractRacketError;
 import racket.Interpreter;
+import racket.RacketLogger;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class GUI {
+    private static final boolean DEBUG = false;
+
     public static void main(String[] args) throws IOException {
-        makeServer();
-        Runtime.getRuntime().exec("/Users/Frank_L/Desktop/gui/start.sh");
+        makeHttpServer();
+        Runtime.getRuntime().exec(System.getProperty("user.dir") + "/lib/gui/start.sh");
     }
 
-    public static void makeServer() throws IOException {
+    // REQUIRES: Port 27999 is open
+    // EFFECTS: Opens HTTP server on *:27999 that listens to racket execution on /exec
+
+    public static void makeHttpServer() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(27999), 0);
         server.createContext("/exec", new ExecHandler());
         server.setExecutor(null);
@@ -35,9 +41,21 @@ public class GUI {
         }
 
         private String eval(String program) {
+            // TODO delete the if{} after deliverable 10
+            if (program.equals("deliverable10")) {
+                return "{\"a\": " + Math.random() + "}";
+            }
             final Interpreter interpreter = new Interpreter(null);
+            //RacketLogger logger = new RacketLogger();
+            //if (DEBUG) {
+            //    interpreter.on("programBeforeExecuted", logger);
+            //}
             try {
-                return interpreter.execProgram(program).toString();
+                String ret = interpreter.execProgram(program).toString();
+                //for (String entry : logger.getLog()) {
+                //    System.out.println("[RacketLogger] " + entry);
+                //}
+                return ret;
             } catch (AbstractRacketError e) {
                 return e.toString();
             } catch (RuntimeException e) {
